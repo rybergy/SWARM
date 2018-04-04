5b52789c19e73c4c35aa17f8d264b5d8a4170872
James-Holden <holdenjames96@gmail.com>
2018-04-04 16:15:05 -0400

    added goal planning outline to sync.py

--- sync.py
+++ sync.py
@@ -1,24 +1,70 @@
-'''
-Sync.py is intended to be a broadcast signal to share personal data 
-with swarm
-
-Current exists to send example message (datetime) to the hard coded 
-xbee address x00x01
-'''
 from xbee import XBee
 from serial import Serial
-import datetime 
+import numpy as np
+from scipy.spatial import Voronoi
 
-PORT = '/dev/ttyUSB0'
-BAUD = 9600
 
-ser = Serial(PORT, BAUD)
+# represents point that has been sampled by a bot, known or communicated
+class SampledPoint:
+    def __init__(self, x, y, elevation):
+        self.x = x
+        self.y = y
+        self.point = (self.x,self.y)
+        self.elevation = elevation  # resource searching for
 
-xbee = XBee(ser)
+    def __repr__(self):
+        return self, self.point, self.elevation
 
-def main(): 
-	print("sync.py")
-	# Send the string 'Hello World' to the module with MY set to 1
-	data = str(datetime.datetime.now())
-	print("sending ", data, " to destination 0001")
-	xbee.tx(dest_addr='\x00\x01', data=data)
+
+# creates and adds new point to known sampledPoints list
+def addPoint(x, y, elevation, sampledPoints):
+    sampledPoints.append(SampledPoint(x, y, elevation))
+
+
+# send point to all known xbees in range
+def broadcastNewPoint(point, connections):
+    pass
+    for bot in connections:
+        sendXBeeMessage(bot, point)
+
+
+
+def sendXBeeMessage(address, data):
+    PORT = '/dev/ttyUSB0'
+    BAUD = 9600
+    ser = Serial(PORT, BAUD)
+    xbee = XBee(ser)
+    xbee.tx(dest_addr='\x00\x01', data=data)
+
+
+# returns a point from vertices, ideally the nearest to current location
+# do not return if x already in sampled, precision radius
+def nextGoal(algorithm, curLocation, sampledPoints, localMax):
+    if algorithm == voronoi:
+        return voronoi(curLocation, sampledPoints, localMax)
+
+
+# convert sampledPoints data into numPy array for voronoi
+# considers vertices of voronoi ridges as new goals
+# return nearest
+def voronoi(curLocation, sampledPoints, localMax):
+    numPoints = np.empty((0,2), int)
+    for sampledPoint in sampledPoints:
+        numPoints = np.append(numPoints, np.array([sampledPoint.point]), axis=0)
+    vertices = Voronoi(numPoints).vertices
+    return vertices[(np.abs(vertices-curLocation)).argmin()]
+
+
+# test
+sampledPoints = []
+curLocation = (2, 4)
+localMax = SampledPoint(10, 10, 100)
+connections = []
+
+addPoint(1, 2, 0, sampledPoints)
+addPoint(3.5, 2.6, 0, sampledPoints)
+addPoint(2, 5, 10, sampledPoints)
+addPoint(4, 5, 6, sampledPoints)
+
+goal = nextGoal(voronoi, curLocation, sampledPoints, localMax)
+print(goal)
