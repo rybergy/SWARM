accc465f5bb2411548506209eccecac367efe352
drowrin <drowrin@gmail.com>
2018-04-19 19:27:27 -0400

    remove superfluous systems from arduino test

--- tests/serial_link.py
+++ tests/serial_link.py
@@ -8,23 +8,23 @@ import swarm
 # load configuration
 config = swarm.config.load()
 
-bot = swarm.bot.Bot(config)
-bot.start()
+a = swarm.communication.Arduino(None, config['arduino'])
+a.start()
 
 # main test
 time.sleep(3)
-bot.arduino.set_mode(0)
-bot.arduino.control(50, 150)
+a.set_mode(0)
+a.control(50, 150)
 time.sleep(1)
-bot.arduino.control(200, 200)
+a.control(200, 200)
 time.sleep(2)
-bot.arduino.control(25, 25)
+a.control(25, 25)
 time.sleep(2)
-bot.arduino.control(0, 0)
-bot.arduino.new_gps(50.14063924666028, -90.94233550243678)
+a.control(0, 0)
+a.new_gps(50.14063924666028, -90.94233550243678)
 while False:
     time.sleep(8)
     state: str = random.choice(['stop', 'forward', 'backward', 'left', 'right', 'auto'])
     print('DIRECTION CHANGE: ' + state)
-    bot.arduino.force_direction(state)
+    a.force_direction(state)
 
